7297049df3ab398d4d1b1fd508c81080ebeed16d
duongne1624 <121754943+duongne1624@users.noreply.github.com>
2025-01-17 19:11:45 +0700

    Init code

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+
+const hostname = 'localhost'
+const port = '8017'
+
+app.get('/', function(req, res) {
+  res.send('<h1>Hello world</h1>')
+})
+
+app.listen(port, hostname, function() {
+  console.log(`Server running at http://${hostname}:${port}/`)
+})
